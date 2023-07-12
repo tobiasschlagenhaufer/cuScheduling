397ed7ff6be83cc715f572e3492a246e42c9b70f
Forest Anderson <forestkzanderson@gmail.com>
2023-07-12 13:42:00 -0400

    Change to dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ env
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f tmp/pids/server.pid
 
-SECRET_KEY_BASE=$(openssl rand -hex 32) RAILS_ENV=production bundle exec rails db:setup --trace
+SECRET_KEY_BASE=$(openssl rand -hex 32) RAILS_ENV=development bundle exec rails db:setup --trace
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
-SECRET_KEY_BASE=$(openssl rand -hex 32) bundle exec rails s -p 3000 -b '0.0.0.0' -e production
+SECRET_KEY_BASE=$(openssl rand -hex 32) bundle exec rails s -p 3000 -b '0.0.0.0' -e development
